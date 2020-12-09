83cc7e00f1edfdc78cea5d249ab356b97ac37016
Ivan Ficko <ificko@span.eu>
2020-12-09 11:53:02 +0100

    Updated docs

--- FastRecordCounter/Properties/AssemblyInfo.cs
+++ FastRecordCounter/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2e32dd94-b93d-4de6-a468-5ecff8e3b9e1")]
-[assembly: AssemblyVersion("1.2020.12.1")]
-[assembly: AssemblyFileVersion("1.2020.12.1")]
+[assembly: AssemblyVersion("1.2020.12.2")]
+[assembly: AssemblyFileVersion("1.2020.12.2")]
